087b1fa3250c88b4c54f90a327d1c4e0a3de8ac0
asolovov <andre.solovov@gmail.com>
2023-02-08 16:43:47 +0400

    deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,19 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    mainnet: {
+      url: process.env.PROVIDER_URL,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    goerli: {
+      url: process.env.PROVIDER_URL,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY as string,
+  }
 };
 
 export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,43 @@
+import { ethers } from "hardhat";
+import hre from "hardhat";
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying OnchainCV contract with the account:", deployer.address);
+
+    const OnchainCV = await ethers.getContractFactory("OnchainCV");
+    const gasPrice = await OnchainCV.signer.getGasPrice();
+    console.log(`Current gas price: ${gasPrice}`);
+    const estimatedGas = await OnchainCV.signer.estimateGas(
+        OnchainCV.getDeployTransaction(),
+    );
+    console.log(`Estimated gas: ${estimatedGas}`);
+    const deploymentPrice = gasPrice.mul(estimatedGas);
+    const deployerBalance = await OnchainCV.signer.getBalance();
+    console.log(`Deployer balance:  ${ethers.utils.formatEther(deployerBalance)}`);
+    console.log(`Deployment price:  ${ethers.utils.formatEther(deploymentPrice)}`);
+    if (Number(deployerBalance) < Number(deploymentPrice)) {
+        throw new Error("You dont have enough balance to deploy.");
+    }
+
+    const cv = await OnchainCV.deploy();
+
+    await cv.deployed();
+
+    console.log("OnchainCV contract deployed to address:", cv.address);
+
+    console.log("Waiting 30 seconds before etherscan verification...");
+    await new Promise(f => setTimeout(f, 30000));
+
+    await hre.run("verify:verify", {
+        address: cv.address,
+    });
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error("Error:", error);
+        process.exit(1);
+    });
